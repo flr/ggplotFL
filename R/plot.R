4dffb41be6c489e3677342028c59a8917c7c9dfd
Iago Mosqueira <iago.mosqueira@gmail.com>
2010-11-02 10:33:07 +0000

    Added empty ggplotFL pkg

---
+++ R/plot.R
@@ -0,0 +1,11 @@
+# plot - «Short one line description»
+# plot
+
+# Copyright 2010 Iago Mosqueira, Cefas. Distributed under the GPL 2 or later
+# $Id:  $
+
+# Reference:
+# Notes:
+
+# TODO Mon 01 Nov 2010 04:31:38 PM CET IM:
+
