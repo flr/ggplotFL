f58fce9f0df1c7d2401cabf51188776db7f2b247
Iago Mosqueira <iago.mosqueira@gmail.com>
2011-05-27 13:25:40 +0000

    Added plot.R

--- R/plot.R
+++ R/plot.R
@@ -1,11 +1,31 @@
-# plot - «Short one line description»
-# plot
+# .R - 
+# /R/.R
 
-# Copyright 2010 Iago Mosqueira, Cefas. Distributed under the GPL 2 or later
+# Copyright 2003-2007 FLR Team. Distributed under the GPL 2 or later
+# Maintainer: Iago Mosqueira, JRC
 # $Id:  $
 
-# Reference:
-# Notes:
+# plot(FLStock) {{{
+setMethod("plot", signature(x="FLStock", y="missing"),
+  function(x, probs=c(0.95,0.50,0.05), size=c(0.5,1.0,0.5), lty=c(2,1,2),
+    facet=facet_wrap(~qname,scale="free"),
+    fn=list("SSB"=ssb, "Recruits" = rec,
+      "Plus Group"=function(x) stock.n(x)[ac(dims(x)$max)],
+      "Fpg"=function(x) harvest(x)[ac(dims(x)$max)],
+      "F2:5"=function(x) apply(harvest(x)[ac(2:5)],2,mean)),...) {
+    
+    plotComp(x,fn,probs,size,lty,facet)
+  }
+) # }}}
 
-# TODO Mon 01 Nov 2010 04:31:38 PM CET IM:
+# plot(FLStocks) {{{
+setMethod("plot", signature(x="FLStocks", y="missing"),
+  function(x, probs=c(0.95,0.50,0.05), size=c(0.5,1.0,0.5), lty=c(2,1,2),
+    facet=facet_wrap(~qname,scale="free"),
+    fn=list("SSB"=ssb, "Recruits"=rec,
+      "Plus Group"=function(x) stock.n(x)[ac(dims(x)$max)],
+      "Fpg"=function(x) harvest(x)[ac(dims(x)$max)],
+      "F2:5"=function(x) apply(harvest(x)[ac(2:5)],2,mean)),...)
 
+    plotComps(x,fn,probs,size,lty,facet)
+) # }}}
