7c34cbdefd3b1163c4a3994c3a9693253e7cba4b
Laurence Kell <flr-team@flr-project.org>
2011-08-24 09:34:11 +0000

    plotComp fixed & quantiles messed with

--- R/plotComp.R
+++ R/plotComp.R
@@ -6,44 +6,54 @@
 # $Id:  $
 
 # whooow {{{
-whooow  <-function(x,fn,probs)
+whooow  =function(x,fn,probs)
   as.data.frame(FLQuants(lapply(fn,
         function(fn,x)
           quantile(fn(x), probs=probs, na.rm=T), x=x))) # }}}
 
 # plotComp {{{
-plotComp <- function(x, fn=NULL, probs=c(0.95,0.50,0.05), size=c(0.5,1.0,0.5),
+plotComp = function(x, fn=NULL, probs=c(0.75,0.50,0.25), size=c(0.5,1.0,0.5),
   lty=c(2,1,2), facet=facet_wrap(~qname, scale="free")) {
+  
+  if (dims(x)$iter>=length(probs)){
+    res = whooow(x,fn,probs)
+    p1  = ggplot(res) + geom_line(aes(x=year,y=data,group=iter,size=iter,lty=iter)) +
+                        scale_size_manual(    values=size, name="Quantile") +
+                        scale_linetype_manual(values=lty , name="Quantile")
+  }else{
+    res = whooow(x,fn,0.5)
+    p1  = ggplot(res) + geom_line(aes(x=year,y=data))    }
 
-  res <- whooow(x,fn,probs)
-
-  #
-  p1 <- ggplot(res) + geom_line(aes(x=year,y=data,group=iter,size=iter,lty=iter)) +
-                    scale_size_manual(    values=size, name="Quantile") +
-                    scale_linetype_manual(values=lty , name="Quantile") +
-                    expand_limits(y = 0) +
-                    xlab("Year") + ylab("") +
-                    facet
-
-   print(p1)
-   invisible(p1)
+  p1 = p1 +  expand_limits(y = 0) +
+             xlab("Year") + ylab("") +
+             facet
+  print(p1)
+  invisible(p1)
 } # }}}
 
 # plotComps {{{
-plotComps <- function(x, fn=NULL, probs=c(0.95,0.50,0.05), size=c(0.5,1.0,0.5),
+plotComps = function(x, fn=NULL, probs=c(0.75,0.50,0.25), size=c(0.5,1.0,0.5),
   lty=c(2,1,2), facet=facet_wrap(~qname,scale="free")) {
 
-  res <- ldply(x, whooow, fn=fn, probs=probs)
-  #
-  res$.id <- factor(res$.id)
-  res$iter <- factor(res$iter)
+  if (max(laply(x,function(x) dims(x)$iter)>=length(probs)))
+    res = ldply(x, whooow, fn=fn, probs=probs)
+  else
+    res = ldply(x, whooow, fn=fn, probs=0.5)
 
-  p1 <- ggplot(res)+ geom_line(aes(x=year,y=data,group=.id:iter,size=iter,col=.id)) +
-                    scale_size_manual(    values=size, name="Quantile") +
-                    scale_linetype_manual(values=lty , name="Quantile") +
-                    expand_limits(y = 0) +
-                    xlab("Year") + ylab("") +
-                    facet
+  if ("X1" %in% names(res)) names(res)[names(res)=="X1"]=".id"
+  res$.id = factor(res$.id)
+  res$iter = factor(res$iter)
+
+  if (length(unique(res$iter))>=length(probs)){
+    p1  = ggplot(res) + geom_line(aes(x=year,y=data,group=.id:iter,size=iter,col=.id)) +
+                        scale_size_manual(    values=size, name="Quantile") +
+                        scale_linetype_manual(values=lty , name="Quantile")
+  }else{
+    p1  = ggplot(res) + geom_line(aes(x=year,y=data,group=.id,col=.id)) 
+    }  
+  p1= p1 + expand_limits(y = 0) +
+           xlab("Year") + ylab("") +
+           facet
 
    print(p1)
    invisible(p1)} 
