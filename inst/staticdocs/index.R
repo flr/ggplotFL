64b2d012e05a61214aaa29c32291fd478216f102
iagomosqueira <iago.mosqueira@gmail.com>
2014-05-20 23:22:24 +0200

    Adding travis

---
+++ inst/staticdocs/index.R
@@ -0,0 +1,16 @@
+# index.R - DESC
+# index.R
+
+# Copyright 2003-2014 FLR Team. Distributed under the GPL 2 or later
+# Maintainer: Iago Mosqueira, JRC
+# Soundtrack:
+# Notes:
+
+sd_section("ggplot",
+  "Overloaded ggplot method for various FLR classes",
+  c("ggplot"))
+
+sd_section("FLR classes plot",
+  "ggplot-based plot for different FLR classes",
+  c("plot"))
+
