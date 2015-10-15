db9434ab55fae9391b51616df87562658f22b65a
Iago Mosqueira <iago@mosqueira.eu>
2015-10-15 14:46:49 +0200

    Change in fill color

--- R/plot.R
+++ R/plot.R
@@ -1,7 +1,7 @@
 # plot.R - ggplot2-based plot methods for FLCore classes
 # ggplotFL/R/plot.R
 
-# Copyright 2003-2014 FLR Team. Distributed under the GPL 2 or later
+# Copyright 2003-2015 FLR Team. Distributed under the GPL 2 or later
 # Maintainer: Iago Mosqueira, JRC, Laurie Kell, ICCAT
 
 # plot(FLQuant) {{{
@@ -116,7 +116,7 @@ setMethod("plot", signature(x="FLQuant", y="missing"),
 
 setMethod("plot", signature(x="FLQuants", y="missing"),
 	function(x, main="", xlab="", ylab="", probs=c(0.10, 0.25, 0.50, 0.75, 0.90),
-		na.rm=TRUE, type=7) {
+		na.rm=TRUE, type=7, fill="red", colour=fill) {
 
 		# check names not repeated
 		dup <- duplicated(names(x))
@@ -155,15 +155,15 @@ setMethod("plot", signature(x="FLQuants", y="missing"),
 			p <- p +
 			# 75% quantile ribbon in red, alpha=0.25
 			geom_ribbon(aes(x=year, ymin = `25%`, ymax = `75%`),
-				fill="red", alpha = .25, na.rm=na.rm) +
+				fill=fill, alpha = .25, na.rm=na.rm) +
 			# 90% quantile ribbon in red, aplha=0.10
 			geom_ribbon(aes(x=year, ymin = `10%`, ymax = `90%`),
-				fill="red", alpha = .10, na.rm=na.rm) +
+				fill=fill, alpha = .10, na.rm=na.rm) +
 			# .. and dotted lines
 			geom_path(aes(x=year, y = `10%`),
-				colour="red", alpha = .50, linetype=3, na.rm=na.rm) +
+				colour=colour, alpha = .50, linetype=3, na.rm=na.rm) +
 			geom_path(aes(x=year, y = `90%`),
-				colour="red", alpha = .50, linetype=3, na.rm=na.rm)
+				colour=colour, alpha = .50, linetype=3, na.rm=na.rm)
 		}
 		
 		return(p)
@@ -204,7 +204,8 @@ setMethod("plot", signature(x="FLStock", y="missing"),
 #'  
 
 setMethod("plot", signature(x="FLStocks", y="missing"),
-	function(x, main="", xlab="", ylab="", na.rm=TRUE, ...) {
+	function(x, main="", xlab="", ylab="", na.rm=TRUE,
+		foo= function(y) FLQuants(Rec=rec(y), SSB=ssb(y), Catch=catch(y), Harvest=fbar(y))) {
 	
 		# check names not repeated
 		dup <- duplicated(names(x))
@@ -214,8 +215,76 @@ setMethod("plot", signature(x="FLStocks", y="missing"),
 		}
 		
 		# extract slots by stock
-		fqs <- lapply(x, function(y) FLQuants(Rec=rec(y), SSB=ssb(y),
-			Catch=catch(y), Harvest=fbar(y)))
+		fqs <- lapply(x, foo)
+
+		# get median & 85% quantiles if iters
+		its <- unlist(lapply(x, function(x) dims(x)$iter))
+		if(any(its > 1))
+		{
+			# quantiles
+			fqs <- lapply(fqs, function(y) as.data.frame(lapply(y, quantile,
+				c(0.10, 0.50, 0.90), na.rm=TRUE)))
+		} else {
+			fqs <- lapply(fqs, as.data.frame)
+			fqs <- lapply(fqs, function(x) {x$iter <- "50%"; return(x)})
+		}
+
+		# stock names
+		stk <- rep.int(names(fqs), unlist(lapply(fqs, nrow)))
+		# rbind dfs
+		fqs <- do.call(rbind, fqs)
+		rownames(fqs) <- NULL
+		# add stock names
+		fqs <- transform(fqs, stock=stk)
+
+		# cast with quantiles in columns
+		df <- dcast(fqs, age+year+unit+season+area+qname+stock~iter, value.var="data")
+
+		# plot data vs. year + facet on qname +
+		p <- ggplot(data=df, aes(x=year, y=`50%`, group=stock)) +
+			facet_grid(qname~., scales="free") +
+			# line + xlab + ylab +
+			geom_line(aes(colour=stock), na.rm=na.rm) + xlab(xlab) + ylab(ylab) +
+			# limits to include 0 +  no legend
+			expand_limits(y=0) + theme(legend.title = element_blank())
+		
+		# object w/ iters?
+		if(any(unlist(lapply(x, function(y) dims(y)$iter)) > 1)) {
+				p <- p +
+			# 75% quantile ribbon in red, alpha=0.25
+			geom_ribbon(aes(x=year, ymin = `10%`, ymax = `90%`, group=stock,
+				colour=stock, fill=stock), alpha = .20, linetype = 0, na.rm=na.rm)
+			# 90% quantile ribbon in red, aplha=0.10
+		}
+		return(p)
+	}
+) # }}}
+
+# plot(FLStocks, FLPar) {{{
+#' @aliases plot,FLStocks,FLPar-method
+#' @rdname plot
+#' @examples
+#'
+#'  # plot for FLStocks
+#'  data(ple4)
+#'  pls <- FLStocks(runA=ple4, runB=qapply(ple4, function(x) x*1.10))
+#'  plot(pls)
+#'  
+
+setMethod("plot", signature(x="FLStocks", y="FLPar"),
+	function(x, y, main="", xlab="", ylab="", na.rm=TRUE,
+		foo= function(x, y) FLQuants(SSB=ssb(x)/y[,'ssb',], F=fbar(x)/y[,'harvest',],
+			Catch=catch(x))) {
+		
+		# check names not repeated
+		dup <- duplicated(names(x))
+		if(any(dup)) {
+			names(x)[dup] <- paste(names(x)[dup], LETTERS[seq(sum(dup))], sep='_')
+			warning('Duplicated names in object, changed to differentiate')
+		}
+		
+		# extract slots by stock
+		fqs <- lapply(x, foo, y)
 
 		# get median & 85% quantiles if iters
 		its <- unlist(lapply(x, function(x) dims(x)$iter))
@@ -318,8 +387,8 @@ setMethod('plot', signature(x='FLSR', y='missing'),
 
 	# SSB vs. REC
 	p1 <- ggplot(data=dat, aes(x=SSB, y=Rec)) + geom_point() +
-		geom_smooth(method='loess') + xlab(uns) +
-		ylab(unr)
+		geom_smooth(method='loess') + xlab(uns) + ylab(unr) +
+		expand_limits(y=0) + expand_limits(x=0)
 
 	# model fit line
 	form <- as.list(model(x))[[3]]
@@ -353,8 +422,12 @@ setMethod('plot', signature(x='FLSR', y='missing'),
 	p6 <- ggplot(data=dat, aes(x=RecHat, y=Residuals)) + geom_point() + 	
 		geom_smooth(method='loess') + xlab(expression(hat(Recruits)))
 	
-	p <- gridExtra::arrangeGrob(p1, p2, p3, p4, p5, p6, ncol=2)
+
+	# BUG Does not return a ggplot, but a grob
+	invisible(grid.arrange(p1, p2, p3, p4, p5, p6, ncol=2))
+
+#	p <- gridExtra::arrangeGrob(p1, p2, p3, p4, p5, p6, ncol=2)
 	
-	return(p)
+#	return(p)
 	}
-) # }}}# plot(FLComps, list)
+) # }}}
